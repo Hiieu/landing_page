f596cc45bac4cca0f6d0fa361e93bd4b95b1f79c
Hiieu <you@example.com>
2023-01-15 09:28:41 +0000

    test comp

--- components/TestBut.tsx
+++ components/TestBut.tsx
@@ -37,6 +37,7 @@ function TestBut(props: TestButProps) {
   // By default, we are just piping all TestButProps here, but feel free
   // to do whatever works for you.
   console.log("HEYYYY!")
+  Object.assign(props, {heyoo: "https://heyo"})
   return <PlasmicTestBut {...props} />;
 }
 
